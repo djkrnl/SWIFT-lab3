0f5055d72b7d1a239e0724a432f6b2a378d18610
Kornel Szarapajew <krnl75313@gmail.com>
2023-10-17 10:19:44 +0200

    v3

--- lab3/ContentView.swift
+++ lab3/ContentView.swift
@@ -1,15 +1,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    let contents: Array<String> = ["a", "b", "c", "d"]
+    let contents: Array<String> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    @State var cardCount = 4
+    let off = 2
 
     var body: some View {
         HStack {
-            ForEach (contents.indices, id: \.self) { index in
+            ForEach (0 ..< cardCount, id: \.self) { index in
                 CardView(faceUp: false, content: contents[index])
             }
         }
         .foregroundColor(.blue)
         .padding()
+        
+        HStack {
+            addCard
+            Spacer()
+            removeCard
+        }
+        .padding()
+    }
+    
+    var addCard: some View {
+        return adjustCardNumber(offset: off, symbol: "+")
+            //.isEnabled(cardCount + off <= contents.count ? true : false)
+    }
+    
+    var removeCard: some View {
+        return adjustCardNumber(offset: off, symbol: "-")
+            //.isEnabled(cardCount - off >= 0 ? true : false)
+    }
+    
+    func adjustCardNumber(offset: Int, symbol: String) -> some View {
+        Button(symbol) {
+            if (symbol == "+") {
+                cardCount = cardCount + offset
+            }
+            else if (symbol == "-") {
+                cardCount = cardCount - offset
+            }
+        }
+        .disabled(symbol == "+" ? (cardCount + off > contents.count ? true : false) : (cardCount - off <= 0 ? true : false))
     }
 }
