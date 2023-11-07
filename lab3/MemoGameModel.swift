a33904e953fcf0d8362fb6d341dd86061fcdbb7f
djkrnl <krnl75313@gmail.com>
2023-11-07 11:26:58 +0100

    v7

---
+++ lab3/MemoGameModel.swift
@@ -0,0 +1,8 @@
+//
+//  MemoGameModel.swift
+//  lab3
+//
+//  Created by student on 07/11/2023.
+//
+
+import Foundation
---
+++ lab3/MemoGameViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MemoGameViewModel.swift
+//  lab3
+//
+//  Created by student on 07/11/2023.
+//
+
+import SwiftUI
+
+struct MemoGameViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoGameViewModel()
+}
