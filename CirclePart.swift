8ee2a0e5ecbe041f9597853e873afefc90babf15
djkrnl <krnl75313@gmail.com>
2023-11-28 11:13:00 +0100

    v9 (WIP)

---
+++ CirclePart.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct CirclePart: Shape {
+    var startAngle: Angle = .zero
+    var endAngle: Angle
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(x: center.x, y: center.y)
+        let clockwise = false
+        
+        var p = Path()
+        
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(center: center,
+                 radius: radius,
+                 startAngle: startAngle,
+                 endAngle: endAngle,
+                 clockwise: clockwise)
+        p.addLine(to: center)
+        p.fill()
+        
+        return p
+    }
+}
